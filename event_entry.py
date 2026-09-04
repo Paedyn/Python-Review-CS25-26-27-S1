2c7dd3b8bdbc50c9df4da0eabce9c459b6e80e29
Pavaki <pavaki190@gmail.com>
2026-09-04 09:46:32 -0600

    Update event_entry.py

--- event_entry.py
+++ event_entry.py
@@ -5,3 +5,9 @@
 #Allow them to enter an event only if they are at least 14 and have a ticket.
 
 age = int(input("What's your age? "))
+ticket= (input("Do you have a ticket?"))
+if age>=14 and ticket== "Yes":
+    print("You can enter the event!")
+else:
+    print("Sorry! You can't go inside!")
+
